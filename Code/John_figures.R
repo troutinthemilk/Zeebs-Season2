2442b2da6552afc9dd16e42fafb7ce9950b04f81
John R Fieberg <jfieberg@ad.umn.edu>
2019-09-17 09:35:48 -0500

    Further updates on showcase presentation

--- Code/John_figures.R
+++ Code/John_figures.R
@@ -1,6 +1,7 @@
 library(wesanderson)
 library(tidyverse)
-load('../Writing/DensEst.Rdata')
+library(scales)
+load('Writing/DensEst.Rdata')
 
 
 #dataf <- data.frame(Lake=rep(c("Lake Florida", "Lake Burgan", "Little Birch Lake"),3), Design=c(rep("Quadrat", 3), rep("Double",3), rep("Distance", 3)), Estimate=c(LF.quadrat.est$Dhat, LB.quadrat.est$Dhat, LBL.quadrat.est.subset$Dhat, LF.double.est$Dhat, LB.double.est$Dhat, LBL.double.est$Dhat, LF.distance.est$Dhat, LB.distance.est$Dhat, LBL.distance.est$Dhat), SE=c(LF.quadrat.est$Dhat.se, LB.quadrat.est$Dhat.se, LBL.quadrat.est.subset$Dhat.se, LF.double.est$Dhat.se, LB.double.est$Dhat.se, LBL.double.est$Dhat.se, LF.distance.est$Dhat.se, LB.distance.est$Dhat.se, LBL.distance.est$Dhat.se))
@@ -10,9 +11,12 @@ dataf <- data.frame(Lake=rep(c("Lake Florida \n (low density)", "Lake Burgan \n 
 dataf$Lake <- relevel(dataf$Lake, "Lake Florida \n (low density)")
 surveyTypeCols <- wes_palette("Darjeeling1")
 
-ggplot(dataf, aes(x=Design, y=Estimate, colour=Design)) + geom_point(size=4) + facet_wrap(~Lake, ncol=3) + geom_errorbar(aes(ymin=Estimate-2*SE, ymax=Estimate+2*SE, width=0)) + labs(x="", y="Density estimate") + theme_classic() + scale_fill_manual(values=surveyTypeCols) + scale_colour_manual(values=surveyTypeCols) + guides(fill=FALSE)
+ggplot(dataf, aes(x=Design, y=Estimate, colour=Design)) + geom_point(size=4) + facet_wrap(~Lake, ncol=3) + geom_errorbar(aes(ymin=pmax(0.000001,Estimate-2*SE), ymax=Estimate+2*SE, width=0)) + labs(x="", y="Density estimate") + theme_classic(base_size=24) + scale_fill_manual(values=surveyTypeCols) + scale_colour_manual(values=surveyTypeCols) + guides(fill=FALSE)+scale_y_continuous(trans = log10_trans(),breaks = trans_breaks("log10", function(x) 10^x), labels = trans_format("log10", math_format(10^.x))) #coord_trans(y='log10')#scale_y_continuous(trans='log10')
 
-ggplot(dataf, aes(x=Design, y=Estimate, colour=Design)) + geom_point(size=4) + facet_wrap(~Lake, ncol=3, scale="free") + geom_errorbar(aes(ymin=Estimate-2*SE, ymax=Estimate+2*SE, width=0)) + labs(x="", y="Density estimate") + theme_classic() + scale_fill_manual(values=surveyTypeCols) + scale_colour_manual(values=surveyTypeCols) + guides(fill=FALSE) #+ scale_y_log10()
+
+ggplot(dataf, aes(x=Design, y=Estimate, colour=Design)) + geom_point(size=4) + facet_wrap(~Lake, ncol=3) + geom_errorbar(aes(ymin=pmax(0.000001,Estimate-2*SE), ymax=Estimate+2*SE, width=0)) + labs(x="", y="Density estimate") + theme_classic(base_size=24) + scale_fill_manual(values=surveyTypeCols) + scale_colour_manual(values=surveyTypeCols) + guides(fill=FALSE)+scale_y_continuous(trans = 'log10',breaks = c(0, 0.01, 1, 10, 100), labels =c("0", "0.1", "1", "10", "100")) #coord_trans(y='log10')#scale_y_continuous(trans='log10')
+
+ggplot(dataf, aes(x=Design, y=Estimate, colour=Design)) + geom_point(size=4) + facet_wrap(~Lake, ncol=3, scale="free") + geom_errorbar(aes(ymin=Estimate-2*SE, ymax=Estimate+2*SE, width=0)) + labs(x="", y="Density estimate") + theme_classic(base_size=24) + scale_fill_manual(values=surveyTypeCols) + scale_colour_manual(values=surveyTypeCols) + guides(fill=FALSE)#+ scale_y_log10()
 
 
 
